092b6d12e79773b8c56c21e830140bd96108e949
rociomonaco <rociomonaco@gmail.com>
2025-05-11 20:59:43 -0300

    add nav scrolled

--- src/components/navbars/NavbarLanding.jsx
+++ src/components/navbars/NavbarLanding.jsx
@@ -1,14 +1,35 @@
+"use client";
 import Link from "next/link";
+import { useState, useEffect} from "react";
+
 
 export default function NavbarLanding() {
+  const [scrolled, setScrolled] = useState(false);
+
+  useEffect(() => {
+    const handleScroll = () => {
+      if (window.scrollY > 0) {
+        setScrolled(true);
+      } else {
+        setScrolled(false);
+      }
+    };
+
+    window.addEventListener("scroll", handleScroll);
+    return () => window.removeEventListener("scroll", handleScroll);
+  }, []);
+
+
   return (
-    <nav className="fixed top-0 left-0 w-full z-50 bg-transparent py-4 px-8">
+    <nav  className={`fixed top-0 left-0 w-full z-50 py-4 px-8 transition-all duration-300 ${
+        scrolled ? "background-color-nav-scrolled shadow text-red-400" : "bg-transparent text-white"
+        }`}>
       <div className="flex justify-between items-center">
-        <div className="text-3xl font-bold text-white">
+        <div className={`text-3xl font-bold ${scrolled ? "text-red-400" : "text-white"}`}>
           <img
-            src="/log.png"
+            src={scrolled ? "/logos/logo-blanco.png" : "/logos/logo-rosa.png"}
             alt="Comida casera"
-            className="w-20 h-20 object-contain"
+            className="w-10 h-10 object-contain"
           />
         </div>
         <div className="space-x-6 text-white">
