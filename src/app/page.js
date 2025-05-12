ad5f4f2aa336a435d53c160f46001989d9af00b6
Walter <wpiedrabuena@gmail.com>
2025-05-12 19:39:57 -0300

    img mobile y fix page

--- src/app/page.js
+++ src/app/page.js
@@ -7,9 +7,9 @@ import Footer from "@/components/Footer";
 import CarrotLoader from "@/components/CarrotLoader";
 export default function Home() {
   return (
-    <div className="min-h-screen"> {/* Esto asegura que el contenedor principal ocupe todo el alto de la pantalla */}
-      <main className="flex flex-col gap-[32px] row-start-2 items-center sm:items-start">
+    <div className="min-h-screen overflow-x-hidden"> {/* Esto asegura que el contenedor principal ocupe todo el alto de la pantalla */}
         <NavbarLanding />
+        <main className="pt-16 flex flex-col gap-16 items-center">
         <PrimerSeccion />  
         <Beneficios />  
         <SobrePlanicocina />
