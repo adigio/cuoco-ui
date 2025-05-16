08fc28c528a3294c16d6efc398e212370edb0be9
Walter <wpiedrabuena@gmail.com>
2025-05-16 17:25:46 -0300

    chef loader

---
+++ src/components/shared/ChefLoader.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ChefLoader.css';
+
+export default function ChefLoader() {
+  return (
+    <div className="loader-container">
+      <img src="/iso_coral.png" alt="Cargando..." className="loader-image" /> 
+    </div>
+  );
+};
+ 
