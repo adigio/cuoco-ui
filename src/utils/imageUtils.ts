1e25f0bc7ef8e1c9c907cc33298e3e5013c3434d
Alan Di Giovanni <alan.digiovanni@redb.ee>
2025-07-13 23:50:07 -0300

    fix[Images]: Try again to fix images

--- src/utils/imageUtils.ts
+++ src/utils/imageUtils.ts
@@ -1,7 +1,7 @@
 import { serverBaseUrl } from '@/lib/config';
 
 export const buildRecipeImageUrl = (recipeId: number | string, imageName: string = 'main.png'): string => {
-  return `${serverBaseUrl}/images/recipe/${recipeId}/${imageName}`;
+  return `/images/recipe/${recipeId}/${imageName}`;
 };
 
 export const getRecipeImageUrl = (
